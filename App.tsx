fd875ec531fba4bed6697cd9386466dc7abe65d9
Silvia Ferrai <silviaferdeveloper@gmail.com>
2023-03-05 18:42:54 +0100

    Add react vector icon

--- App.tsx
+++ App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import { Text } from 'react-native';
+import Icon from 'react-native-vector-icons/Ionicons';
 
 const App = () => {
   return (
     <>
       <Text>App State</Text>
+      <Icon name="star-outline" color="red" size={40} />
     </>
   );
 };
