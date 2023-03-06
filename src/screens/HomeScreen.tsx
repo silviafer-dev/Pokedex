7bfc0fdb654433cdfbda791caa6706d68a39a736
Silvia Ferrai <silviaferdeveloper@gmail.com>
2023-03-06 16:10:59 +0100

    Add basic style in home page

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,10 +1,19 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { Image, Text } from 'react-native';
+import { useSafeAreaInsets } from 'react-native-safe-area-context';
+import { styles } from '../theme/appTheme';
 
 export const HomeScreen = () => {
+  const { top } = useSafeAreaInsets();
   return (
-    <View>
-      <Text>HomeScreen</Text>
-    </View>
+    <>
+      <Image
+        source={require('../assets/pokebola.png')}
+        style={styles.pokeballBG}
+      />
+      <Text style={{ ...styles.title, ...styles.globalMargin, top: top + 20 }}>
+        Pokedex
+      </Text>
+    </>
   );
 };
---
+++ src/theme/appTheme.tsx
@@ -0,0 +1,20 @@
+import { StyleSheet } from 'react-native';
+
+export const styles = StyleSheet.create({
+  globalMargin: {
+    marginHorizontal: 20,
+  },
+  pokeballBG: {
+    width: 300,
+    height: 300,
+    position: 'absolute',
+    top: -100,
+    right: -100,
+    opacity: 0.2,
+  },
+  title: {
+    fontSize: 35,
+    fontWeight: 'bold',
+    color: 'black',
+  },
+});
