010cdaad63b3b7427270166ef054dfc330278bfc
Silvia Ferrai <silviaferdeveloper@gmail.com>
2023-03-07 19:19:54 +0100

    Add interface folder for interface api get request

--- src/hooks/usePokemonPaginated.tsx
+++ src/hooks/usePokemonPaginated.tsx
@@ -1,12 +1,15 @@
 import { useEffect, useRef } from 'react';
 import { pokemonApi } from '../api/pokemonApi';
+import { PokemonPaginatedResponse } from '../interfaces/pokemonInterfaces';
 
 export const usePokemonPaginated = () => {
   const nextPageUrl = useRef('https://pokeapi.co/api/v2/pokemon?limit=40');
 
   const loadPokemons = async () => {
-    const resp = await pokemonApi.get(nextPageUrl.current);
-    console.log(resp.data);
+    const resp = await pokemonApi.get<PokemonPaginatedResponse>(
+      nextPageUrl.current,
+    );
+    nextPageUrl.current = resp.data.next;
   };
   useEffect(() => {
     loadPokemons();
